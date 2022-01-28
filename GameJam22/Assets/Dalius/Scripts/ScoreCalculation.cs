5ceb4440d74f37f3bd7817b2e167343a86daea5b
Dalius <dalius.luksa1@gmail.com>
2022-01-29 01:07:54 +0200

    Added score mechanic

---
+++ GameJam22/Assets/Dalius/Scripts/ScoreCalculation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreCalculation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private float score = 0;
+
+
+    private void Update()
+    {
+        scoreText.text = $"Score: {(int) score}";
+    }
+
+    private void FixedUpdate()
+    {
+        score += Time.fixedDeltaTime;
+    }
+}
