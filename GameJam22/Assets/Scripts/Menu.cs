9066e256ef8a13c2bd744967faa89c8ac56cf803
Dalius <dalius.luksa1@gmail.com>
2022-01-28 19:03:16 +0200

    Project structure and simple Main Menu

---
+++ GameJam22/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        // Start the game
+    }
+
+    public void SettingsButton()
+    {
+        // Open the settings menu
+    }
+
+    public void CreditsButton()
+    {
+        // Open the credits menu
+    }
+
+    public void ExitButton()
+    {
+        // Exit the game
+        Application.Quit();
+    }
+}
